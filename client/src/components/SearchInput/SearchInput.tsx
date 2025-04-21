38762df9e9f17d81fd83a5d872be4a867103ea10
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-04-21 20:46:15 +0300

    add SearchInput component with styles and integrate react-icons

---
+++ client/src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,17 @@
+﻿import React, {FormEventHandler} from 'react';
+import styles from './styles.module.css';
+import { FaSearch } from 'react-icons/fa';
+
+type SearchFormProps = {
+    onSub:  FormEventHandler<HTMLFormElement>;
+};
+export const SearchInput: React.FC<SearchFormProps> = ({ onSub }) => (
+    <form onSubmit={onSub} className={styles.fgsearch}>
+        <input type="text" placeholder="Queen" className={styles.input} />
+        <button type="submit">
+            <FaSearch size={20} />
+        </button>
+    </form>
+);
+
+
