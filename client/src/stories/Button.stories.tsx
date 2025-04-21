2f21f7362998282eff527ef454330faf4e2ae85e
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2025-04-21 11:18:48 +0300

    add button

---
+++ client/src/stories/Button.stories.tsx
@@ -0,0 +1,36 @@
+﻿import type {Meta, StoryObj} from '@storybook/react';
+
+import {Button} from '../components/Button/Button';
+
+const meta: Meta<typeof Button> = {
+    component: Button,
+    title: 'Example/Button',
+    tags: ['autodocs'],
+    argTypes: {
+        onClick: {action: 'clicked'},
+        type: {
+            options: ['default', 'inverse'],
+            control: {type: 'select'},
+        },
+        title: {
+            control: {type: 'text'},
+        },
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        title: 'Button',
+        type: 'default',
+    },
+};
+
+export const Inverse: Story = {
+    args: {
+        title: 'Button',
+        type: 'inverse',
+    },
+};
